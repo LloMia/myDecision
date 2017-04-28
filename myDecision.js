d9eac1100d7cc3c2f55012875ccee747a0a34975
Loyiso <Loyiso@projectcodex.co>
2017-04-28 16:27:07 +0200

    save changes

--- myDecision.js
+++ myDecision.js
@@ -1,18 +1,71 @@
-var output = document.querySelector('.output');
+var output = document.querySelector('.tasks');
 
 var source = document.getElementById('output').innerHTML;
 var template = Handlebars.compile(source);
 
-var tasks = [
-  // {taskName: 'Task 1', todo: false, doing: true, done: false},
-  // {taskName: 'Task 2', todo: false, doing: false, done: true}
-];
+var taskTable = document.querySelector('.tasks');
+
+output.innerHTML = template({
+    tasks: tasks
+});
+
+var tasks = [];
+
+taskTable.addEventListener('click', function(evt) {
+
+      //alert(evt.target.dataset.taskId);
+
+      var clickOnTaskId = evt.target.dataset.taskId;
+
+      //alert(clickOnTaskId);
+
+      //find the task for the clickOnTaskId in tasks
+      var task = null;
+
+      for (var i = 0; i < tasks.length; i++) {
+          if (tasks[i].id === Number(clickOnTaskId)) {
+              task = tasks[i]
+          }
+      }
+
+      //update it's state to doing
+
+      if (task) {
+          task.todo = false;
+          task.doing = true;
+          task.done = false;
+      }
+      //re-render the screen
+      output.innerHTML = template({
+          tasks: tasks
+      });
+
+});
+
+var taskId = 0;
 
 function addText() {
-  var input = document.querySelector('#textField').value;
-  console.log(input);
+    var input = document.querySelector('#textField').value;
+    //console.log(input);
 
-  tasks.push({taskName: input, todo: true, doing: false, done: false});
-  var table = template({tasks: tasks });
-  output.innerHTML = table;
+    taskId += 1;
+
+    tasks.push({
+        id: taskId,
+        taskName: input,
+        todo: true,
+        doing: false,
+        done: false
+    });
+    var table = template({
+        tasks: tasks
+    });
+    output.innerHTML = table;
+}
+
+function start() {
+    alert("add not found")
+    if (tasks.input === tasks.todo) {
+        tasks.classList.add(tasks.doing)
+    }
 }
